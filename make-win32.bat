a6d8ae64c485ef96f81dc3d6478a746e34474030
solstacoded <52147022+sSolsta@users.noreply.github.com>
2025-03-21 14:03:49 +1100

    builds, nothing happens when trying to run

--- make-win32.bat
+++ make-win32.bat
@@ -1,4 +1,12 @@
 :: running cmake with vs2022
 
 cmake -A win32 -S . -B "cmakew32" -T v140
-cmake --build cmakew32
+
+cmake --build cmakew32 --config Debug
+cmake --build cmakew32 --config Release
+
+set DEST=cmakew32\Release
+
+xcopy dlls\ %DEST% /e /y
+xcopy samples\Cpp\HelloCpp\Resources %DEST%\Resources /e /y
+
